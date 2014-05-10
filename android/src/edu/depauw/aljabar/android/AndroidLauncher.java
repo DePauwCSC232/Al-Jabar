fe8c51c037efb9ed267143954baf4d58fbd374e2
Brian Howard <bhoward@depauw.edu>
2014-05-10 10:20:32 -0400

    Initial commit

---
+++ android/src/edu/depauw/aljabar/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package edu.depauw.aljabar.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import edu.depauw.aljabar.AlJabarGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AlJabarGame(), config);
+	}
+}
---
+++ desktop/src/edu/depauw/aljabar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.depauw.aljabar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.depauw.aljabar.AlJabarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AlJabarGame(), config);
+	}
+}
